54561c55eacef7db6282ce8a3d64292afc69d9f9
Justin A-M <justin@omnibook.com>
2023-02-22 10:22:30 +0100

    wip

--- src/app/context/ws.js
+++ src/app/context/ws.js
@@ -3,5 +3,7 @@ import socketio from "socket.io-client";
 import { createContext } from "react";
 const ENDPOINT = process.env.REACT_APP_BACK;
 
+console.log("CONNECT TO BACKEND: ", ENDPOINT);
+
 export const socket = socketio.connect(ENDPOINT);
 export const SocketContext = createContext();
