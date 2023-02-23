4d4e8c9c3211151c611c6a1459c642e24b5d1e9d
Justin A-M <justin@omnibook.com>
2023-02-23 09:02:23 +0100

    fix: back vars

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM node:18.14-alpine
-# ENV REACT_APP_BACK=""
 WORKDIR /app
 COPY ["package.json", "package-lock.json*", "./"]
 RUN npm install
--- src/app/context/ws.js
+++ src/app/context/ws.js
@@ -1,9 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import socketio from "socket.io-client";
 import { createContext } from "react";
-const ENDPOINT = process.env.REACT_APP_BACK;
+const HOST = process.env.REACT_APP_BACK_HOST;
+const PORT = process.env.REACT_APP_BACK_PORT;
 
-console.log("CONNECT TO BACKEND: ", ENDPOINT);
+console.log("CONNECT TO BACKEND: ", `http://${HOST}:${PORT}`);
 
-export const socket = socketio.connect(ENDPOINT);
+export const socket = socketio.connect(`http://${HOST}:${PORT}`);
 export const SocketContext = createContext();
