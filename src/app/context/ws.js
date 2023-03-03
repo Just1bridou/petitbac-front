89b19b5d8eeba52ab3e559e3a5d0d1bd806c20b3
Justin A-M <justin@omnibook.com>
2023-03-03 13:29:33 +0100

    fix: https

--- src/app/context/ws.js
+++ src/app/context/ws.js
@@ -4,7 +4,8 @@ import { createContext } from "react";
 const HOST = process.env.REACT_APP_BACK_HOST;
 const PORT = process.env.REACT_APP_BACK_PORT;
 
-console.log("CONNECT TO BACKEND: ", `http://${HOST}:${PORT}`);
+//let conStr = `http://${HOST}:${PORT}`
+let conStr = `https://${HOST}`
 
-export const socket = socketio.connect(`http://${HOST}:${PORT}`);
+export const socket = socketio.connect(conStr);
 export const SocketContext = createContext();
